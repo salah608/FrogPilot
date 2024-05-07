hoomoose/openpilot

Device Management
Added toggles to customize the device shut down time, disable logging, disable uploads, disable internet connection check, and set a custom battery threshold check to help prevent battery drain.

common/params.cc
@@ -246,6 +246,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CustomTheme", PERSISTENT},
     {"CydiaTune", PERSISTENT},
     {"DecelerationProfile", PERSISTENT},
+    {"DeviceManagement", PERSISTENT},
+    {"DeviceShutdown", PERSISTENT},
     {"DisengageVolume", PERSISTENT},
     {"DynamicPathWidth", PERSISTENT},
     {"EngageVolume", PERSISTENT},
@@ -258,8 +260,13 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"LaneLinesWidth", PERSISTENT},
     {"LateralTune", PERSISTENT},
     {"LongitudinalTune", PERSISTENT},
+    {"LowVoltageShutdown", PERSISTENT},
     {"ManualUpdateInitiated", PERSISTENT},
     {"ModelUI", PERSISTENT},
+    {"MuteOverheated", PERSISTENT},
+    {"NoLogging", PERSISTENT},
+    {"NoUploads", PERSISTENT},
+    {"OfflineMode", PERSISTENT},
     {"PathEdgeWidth", PERSISTENT},
     {"PathWidth", PERSISTENT},
     {"PauseAOLOnBrake", PERSISTENT},

selfdrive/frogpilot/ui/qt/offroad/control_settings.cc
@@ -17,6 +17,14 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
 
     {"CustomPersonalities", tr("Custom Driving Personalities"), tr("Customize the driving personality profiles to your driving style."), "../frogpilot/assets/toggle_icons/icon_custom.png"},
 
+    {"DeviceManagement", tr("Device Management"), tr("Tweak your device's behaviors to your personal preferences."), "../frogpilot/assets/toggle_icons/icon_device.png"},
+    {"MuteOverheated", tr("Bypass Thermal Safety Limits"), tr("Allow the device to run at any temperature even above comma's recommended thermal limits."), ""},
+    {"DeviceShutdown", tr("Device Shutdown Timer"), tr("Configure how quickly the device shuts down after going offroad."), ""},
+    {"NoLogging", tr("Disable Logging"), tr("Turn off all data tracking to enhance privacy or reduce thermal load."), ""},
+    {"NoUploads", tr("Disable Uploads"), tr("Turn off all data uploads to comma's servers."), ""},
+    {"LowVoltageShutdown", tr("Low Voltage Shutdown Threshold"), tr("Automatically shut the device down when your battery reaches a specific voltage level to prevent killing your battery."), ""},
+    {"OfflineMode", tr("Offline Mode"), tr("Allow the device to be offline indefinitely."), ""},
+
     {"LateralTune", tr("Lateral Tuning"), tr("Modify openpilot's steering behavior."), "../frogpilot/assets/toggle_icons/icon_lateral_tune.png"},
 
     {"LongitudinalTune", tr("Longitudinal Tuning"), tr("Modify openpilot's acceleration and braking behavior."), "../frogpilot/assets/toggle_icons/icon_longitudinal_tune.png"},
@@ -130,6 +138,24 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
       relaxedProfile = new FrogPilotDualParamControl(relaxedFollow, relaxedJerk, this, true);
       addItem(relaxedProfile);
 
+    } else if (param == "DeviceManagement") {
+      FrogPilotParamManageControl *deviceManagementToggle = new FrogPilotParamManageControl(param, title, desc, icon, this);
+      QObject::connect(deviceManagementToggle, &FrogPilotParamManageControl::manageButtonClicked, this, [this]() {
+        openParentToggle();
+        for (auto &[key, toggle] : toggles) {
+          toggle->setVisible(deviceManagementKeys.find(key.c_str()) != deviceManagementKeys.end());
+        }
+      });
+      toggle = deviceManagementToggle;
+    } else if (param == "DeviceShutdown") {
+      std::map<int, QString> shutdownLabels;
+      for (int i = 0; i <= 33; ++i) {
+        shutdownLabels[i] = i == 0 ? tr("5 mins") : i <= 3 ? QString::number(i * 15) + tr(" mins") : QString::number(i - 3) + (i == 4 ? tr(" hour") : tr(" hours"));
+      }
+      toggle = new FrogPilotParamValueControl(param, title, desc, icon, 0, 33, shutdownLabels, this, false);
+    } else if (param == "LowVoltageShutdown") {
+      toggle = new FrogPilotParamValueControl(param, title, desc, icon, 11.8, 12.5, std::map<int, QString>(), this, false, tr(" volts"), 1, 0.01);
+
     } else if (param == "LateralTune") {
       FrogPilotParamManageControl *lateralTuneToggle = new FrogPilotParamManageControl(param, title, desc, icon, this);
       QObject::connect(lateralTuneToggle, &FrogPilotParamManageControl::manageButtonClicked, this, [this]() {
@@ -209,6 +235,30 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
     if (params.getBool("AlwaysOnLateralMain")) {
       FrogPilotConfirmationDialog::toggleAlert(
       tr("WARNING: This isn't guaranteed to work, so if you run into any issues, please report it in the FrogPilot Discord!"),
+      tr("I understand the risks."), this);
+    }
+  });
+
+  QObject::connect(toggles["MuteOverheated"], &ToggleControl::toggleFlipped, [this]() {
+    if (params.getBool("MuteOverheated")) {
+      FrogPilotConfirmationDialog::toggleAlert(
+      tr("WARNING: This can cause premature wear or damage by running the device over comma's recommended temperature limits!"),
+      tr("I understand the risks."), this);
+    }
+  });
+
+  QObject::connect(toggles["NoLogging"], &ToggleControl::toggleFlipped, [this]() {
+    if (params.getBool("NoLogging")) {
+      FrogPilotConfirmationDialog::toggleAlert(
+      tr("WARNING: This will prevent your drives from being recorded and the data will be unobtainable!"),
+      tr("I understand the risks."), this);
+    }
+  });
+
+  QObject::connect(toggles["NoUploads"], &ToggleControl::toggleFlipped, [this]() {
+    if (params.getBool("NoUploads")) {
+      FrogPilotConfirmationDialog::toggleAlert(
+      tr("WARNING: This will prevent your drives from appearing on comma connect which may impact debugging and support!"),
       tr("I understand the risks."), this);
     }
   });

selfdrive/frogpilot/ui/qt/offroad/control_settings.h
@@ -33,7 +33,7 @@ private:
 
   std::set<QString> aolKeys = {"AlwaysOnLateralMain", "HideAOLStatusBar", "PauseAOLOnBrake"};
   std::set<QString> conditionalExperimentalKeys = {"CECurves", "CECurvesLead", "CENavigation", "CESignal", "CESlowerLead", "CEStopLights", "HideCEMStatusBar"};
-  std::set<QString> deviceManagementKeys = {};
+  std::set<QString> deviceManagementKeys = {"DeviceShutdown", "LowVoltageShutdown", "MuteOverheated", "NoLogging", "NoUploads", "OfflineMode"};
   std::set<QString> experimentalModeActivationKeys = {};
   std::set<QString> laneChangeKeys = {};
   std::set<QString> lateralTuneKeys = {};

selfdrive/thermald/power_monitoring.py
@@ -37,6 +37,11 @@ class PowerMonitoring:
     self.car_battery_capacity_uWh = max((CAR_BATTERY_CAPACITY_uWh / 10), int(car_battery_capacity_uWh))
 
     # FrogPilot variables
+    device_management = self.params.get_bool("DeviceManagement")
+    device_shutdown_setting = self.params.get_int("DeviceShutdown") if device_management else 33
+    # If the toggle is set for < 1 hour, configure by 15 minute increments
+    self.device_shutdown_time = (device_shutdown_setting - 3) * 3600 if device_shutdown_setting >= 4 else device_shutdown_setting * (60 * 15)
+    self.low_voltage_shutdown = self.params.get_int("LowVoltageShutdown") if device_management else VBATT_PAUSE_CHARGING
 
   # Calculation tick
   def calculate(self, voltage: int | None, ignition: bool):
@@ -116,9 +121,9 @@ class PowerMonitoring:
     now = time.monotonic()
     should_shutdown = False
     offroad_time = (now - offroad_timestamp)
-    low_voltage_shutdown = (self.car_voltage_mV < (VBATT_PAUSE_CHARGING * 1e3) and
+    low_voltage_shutdown = (self.car_voltage_mV < (self.low_voltage_shutdown * 1e3) and
                             offroad_time > VOLTAGE_SHUTDOWN_MIN_OFFROAD_TIME_S)
-    should_shutdown |= offroad_time > MAX_TIME_OFFROAD_S
+    should_shutdown |= offroad_time > self.device_shutdown_time
     should_shutdown |= low_voltage_shutdown
     should_shutdown |= (self.car_battery_capacity_uWh <= 0)
     should_shutdown &= not ignition

salah608/FrogPilot

common/params.cc
@@ -247,6 +247,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CustomTheme", PERSISTENT},
     {"CydiaTune", PERSISTENT},
     {"DecelerationProfile", PERSISTENT},
+    {"DeviceManagement", PERSISTENT},
+    {"DeviceShutdown", PERSISTENT},
     {"DisengageVolume", PERSISTENT},
     {"DynamicPathWidth", PERSISTENT},
     {"EngageVolume", PERSISTENT},
@@ -256,11 +258,16 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"HideAOLStatusBar", PERSISTENT},
     {"HideCEMStatusBar", PERSISTENT},
     {"HideLeadMarker", PERSISTENT},
+    {"IncreaseThermalLimits", PERSISTENT},
     {"LaneLinesWidth", PERSISTENT},
     {"LateralTune", PERSISTENT},
     {"LongitudinalTune", PERSISTENT},
+    {"LowVoltageShutdown", PERSISTENT},
     {"ManualUpdateInitiated", PERSISTENT},
     {"ModelUI", PERSISTENT},
+    {"NoLogging", PERSISTENT},
+    {"NoUploads", PERSISTENT},
+    {"OfflineMode", PERSISTENT},
     {"PathEdgeWidth", PERSISTENT},
     {"PathWidth", PERSISTENT},
     {"PauseAOLOnBrake", PERSISTENT},

selfdrive/frogpilot/ui/qt/offroad/control_settings.cc
@@ -20,6 +20,14 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
 
     {"CustomPersonalities", tr("Custom Driving Personalities"), tr("Customize the driving personality profiles to your driving style."), "../frogpilot/assets/toggle_icons/icon_custom.png"},
 
+    {"DeviceManagement", tr("Device Management"), tr("Tweak your device's behaviors to your personal preferences."), "../frogpilot/assets/toggle_icons/icon_device.png"},
+    {"DeviceShutdown", tr("Device Shutdown Timer"), tr("Configure how quickly the device shuts down after going offroad."), ""},
+    {"NoLogging", tr("Disable Logging"), tr("Turn off all data tracking to enhance privacy or reduce thermal load."), ""},
+    {"NoUploads", tr("Disable Uploads"), tr("Turn off all data uploads to comma's servers."), ""},
+    {"IncreaseThermalLimits", tr("Increase Thermal Safety Limit"), tr("Allow the device to run at a temperature above comma's recommended thermal limits."), ""},
+    {"LowVoltageShutdown", tr("Low Voltage Shutdown Threshold"), tr("Automatically shut the device down when your battery reaches a specific voltage level to prevent killing your battery."), ""},
+    {"OfflineMode", tr("Offline Mode"), tr("Allow the device to be offline indefinitely."), ""},
+
     {"LateralTune", tr("Lateral Tuning"), tr("Modify openpilot's steering behavior."), "../frogpilot/assets/toggle_icons/icon_lateral_tune.png"},
 
     {"LongitudinalTune", tr("Longitudinal Tuning"), tr("Modify openpilot's acceleration and braking behavior."), "../frogpilot/assets/toggle_icons/icon_longitudinal_tune.png"},
@@ -135,6 +143,24 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
       relaxedProfile = new FrogPilotDualParamControl(relaxedFollow, relaxedJerk, this, true);
       addItem(relaxedProfile);
 
+    } else if (param == "DeviceManagement") {
+      FrogPilotParamManageControl *deviceManagementToggle = new FrogPilotParamManageControl(param, title, desc, icon, this);
+      QObject::connect(deviceManagementToggle, &FrogPilotParamManageControl::manageButtonClicked, this, [this]() {
+        openParentToggle();
+        for (auto &[key, toggle] : toggles) {
+          toggle->setVisible(deviceManagementKeys.find(key.c_str()) != deviceManagementKeys.end());
+        }
+      });
+      toggle = deviceManagementToggle;
+    } else if (param == "DeviceShutdown") {
+      std::map<int, QString> shutdownLabels;
+      for (int i = 0; i <= 33; ++i) {
+        shutdownLabels[i] = i == 0 ? tr("5 mins") : i <= 3 ? QString::number(i * 15) + tr(" mins") : QString::number(i - 3) + (i == 4 ? tr(" hour") : tr(" hours"));
+      }
+      toggle = new FrogPilotParamValueControl(param, title, desc, icon, 0, 33, shutdownLabels, this, false);
+    } else if (param == "LowVoltageShutdown") {
+      toggle = new FrogPilotParamValueControl(param, title, desc, icon, 11.8, 12.5, std::map<int, QString>(), this, false, tr(" volts"), 1, 0.01);
+
     } else if (param == "LateralTune") {
       FrogPilotParamManageControl *lateralTuneToggle = new FrogPilotParamManageControl(param, title, desc, icon, this);
       QObject::connect(lateralTuneToggle, &FrogPilotParamManageControl::manageButtonClicked, this, [this]() {
@@ -214,6 +240,30 @@ FrogPilotControlsPanel::FrogPilotControlsPanel(SettingsWindow *parent) : FrogPil
     if (params.getBool("AlwaysOnLateralMain")) {
       FrogPilotConfirmationDialog::toggleAlert(
       tr("WARNING: This isn't guaranteed to work, so if you run into any issues, please report it in the FrogPilot Discord!"),
+      tr("I understand the risks."), this);
+    }
+  });
+
+  QObject::connect(toggles["IncreaseThermalLimits"], &ToggleControl::toggleFlipped, [this]() {
+    if (params.getBool("IncreaseThermalLimits")) {
+      FrogPilotConfirmationDialog::toggleAlert(
+      tr("WARNING: This can cause premature wear or damage by running the device over comma's recommended temperature limits!"),
+      tr("I understand the risks."), this);
+    }
+  });
+
+  QObject::connect(toggles["NoLogging"], &ToggleControl::toggleFlipped, [this]() {
+    if (params.getBool("NoLogging")) {
+      FrogPilotConfirmationDialog::toggleAlert(
+      tr("WARNING: This will prevent your drives from being recorded and the data will be unobtainable!"),
+      tr("I understand the risks."), this);
+    }
+  });
+
+  QObject::connect(toggles["NoUploads"], &ToggleControl::toggleFlipped, [this]() {
+    if (params.getBool("NoUploads")) {
+      FrogPilotConfirmationDialog::toggleAlert(
+      tr("WARNING: This will prevent your drives from appearing on comma connect which may impact debugging and support!"),
       tr("I understand the risks."), this);
     }
   });

selfdrive/frogpilot/ui/qt/offroad/control_settings.h
@@ -33,7 +33,7 @@ private:
 
   std::set<QString> aolKeys = {"AlwaysOnLateralMain", "HideAOLStatusBar", "PauseAOLOnBrake"};
   std::set<QString> conditionalExperimentalKeys = {"CECurves", "CECurvesLead", "CENavigation", "CESignal", "CESlowerLead", "CEStopLights", "HideCEMStatusBar"};
-  std::set<QString> deviceManagementKeys = {};
+  std::set<QString> deviceManagementKeys = {"DeviceShutdown", "IncreaseThermalLimits", "LowVoltageShutdown", "NoLogging", "NoUploads", "OfflineMode"};
   std::set<QString> experimentalModeActivationKeys = {};
   std::set<QString> laneChangeKeys = {};
   std::set<QString> lateralTuneKeys = {};

selfdrive/thermald/power_monitoring.py
@@ -37,6 +37,11 @@ class PowerMonitoring:
     self.car_battery_capacity_uWh = max((CAR_BATTERY_CAPACITY_uWh / 10), int(car_battery_capacity_uWh))
 
     # FrogPilot variables
+    device_management = self.params.get_bool("DeviceManagement")
+    device_shutdown_setting = self.params.get_int("DeviceShutdown") if device_management else 33
+    # If the toggle is set for < 1 hour, configure by 15 minute increments
+    self.device_shutdown_time = (device_shutdown_setting - 3) * 3600 if device_shutdown_setting >= 4 else device_shutdown_setting * (60 * 15)
+    self.low_voltage_shutdown = self.params.get_float("LowVoltageShutdown") if device_management else VBATT_PAUSE_CHARGING
 
   # Calculation tick
   def calculate(self, voltage: int | None, ignition: bool):
@@ -116,9 +121,9 @@ class PowerMonitoring:
     now = time.monotonic()
     should_shutdown = False
     offroad_time = (now - offroad_timestamp)
-    low_voltage_shutdown = (self.car_voltage_mV < (VBATT_PAUSE_CHARGING * 1e3) and
+    low_voltage_shutdown = (self.car_voltage_mV < (self.low_voltage_shutdown * 1e3) and
                             offroad_time > VOLTAGE_SHUTDOWN_MIN_OFFROAD_TIME_S)
-    should_shutdown |= offroad_time > MAX_TIME_OFFROAD_S
+    should_shutdown |= offroad_time > self.device_shutdown_time
     should_shutdown |= low_voltage_shutdown
     should_shutdown |= (self.car_battery_capacity_uWh <= 0)
     should_shutdown &= not ignition