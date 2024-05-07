mike86437/openpilot

FrogPilot setup

selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -1,0 +1,84 @@
+import numpy as np
+
+import cereal.messaging as messaging
+
+from openpilot.common.conversions import Conversions as CV
+from openpilot.common.numpy_fast import interp
+from openpilot.common.params import Params
+
+from openpilot.selfdrive.car.interfaces import ACCEL_MIN, ACCEL_MAX
+from openpilot.selfdrive.controls.lib.drive_helpers import V_CRUISE_MAX
+from openpilot.selfdrive.controls.lib.longitudinal_mpc_lib.long_mpc import COMFORT_BRAKE, STOP_DISTANCE, get_jerk_factor, get_safe_obstacle_distance, get_stopped_equivalence_factor, get_T_FOLLOW
+from openpilot.selfdrive.controls.lib.longitudinal_planner import A_CRUISE_MIN, get_max_accel
+
+from openpilot.system.version import get_short_branch
+
+from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_functions import CITY_SPEED_LIMIT, CRUISING_SPEED, calculate_lane_width, calculate_road_curvature
+
+class FrogPilotPlanner:
+  def __init__(self, CP):
+    self.CP = CP
+
+    self.params = Params()
+    self.params_memory = Params("/dev/shm/params")
+
+    self.staging = get_short_branch() in ["FrogPilot-Development", "FrogPilot-Staging", "FrogPilot-Testing"]
+
+    self.jerk = 0
+    self.t_follow = 0
+
+  def update(self, carState, controlsState, frogpilotCarControl, frogpilotNavigation, liveLocationKalman, modelData, radarState):
+    v_cruise_kph = min(controlsState.vCruise, V_CRUISE_MAX)
+    v_cruise = v_cruise_kph * CV.KPH_TO_MS
+    v_ego = max(carState.vEgo, 0)
+    v_lead = radarState.leadOne.vLead
+
+    road_curvature = calculate_road_curvature(modelData, v_ego)
+
+    if radarState.leadOne.status and self.CP.openpilotLongitudinalControl:
+      base_jerk = get_jerk_factor(controlsState.personality)
+      base_t_follow = get_T_FOLLOW(controlsState.personality)
+      self.jerk, self.t_follow = self.update_follow_values(base_jerk, radarState, base_t_follow, v_ego, v_lead)
+    else:
+      self.t_follow = 1.45
+
+    self.v_cruise = self.update_v_cruise(carState, controlsState, controlsState.enabled, liveLocationKalman, modelData, road_curvature, v_cruise, v_ego)
+
+  def update_follow_values(self, jerk, radarState, t_follow, v_ego, v_lead):
+    lead_distance = radarState.leadOne.dRel
+
+    return jerk, t_follow
+
+  def update_v_cruise(self, carState, controlsState, enabled, liveLocationKalman, modelData, road_curvature, v_cruise, v_ego):
+    gps_check = liveLocationKalman.gpsOK and liveLocationKalman.inputsOK
+
+    v_cruise_cluster = max(controlsState.vCruiseCluster, controlsState.vCruise) * CV.KPH_TO_MS
+    v_cruise_diff = v_cruise_cluster - v_cruise
+
+    v_ego_cluster = max(carState.vEgoCluster, v_ego)
+    v_ego_diff = v_ego_cluster - v_ego
+
+    targets = []
+    filtered_targets = [target if target > CRUISING_SPEED else v_cruise for target in targets]
+
+    return min(filtered_targets)
+
+  def publish(self, sm, pm):
+    frogpilot_plan_send = messaging.new_message('frogpilotPlan')
+    frogpilot_plan_send.valid = sm.all_checks(service_list=['carState', 'controlsState'])
+    frogpilotPlan = frogpilot_plan_send.frogpilotPlan
+
+    frogpilotPlan.jerk = float(self.jerk)
+    frogpilotPlan.tFollow = float(self.t_follow)
+    frogpilotPlan.vCruise = float(self.v_cruise)
+
+    pm.send('frogpilotPlan', frogpilot_plan_send)
+
+  def update_frogpilot_params(self):
+    self.is_metric = self.params.get_bool("IsMetric")
+
+    custom_alerts = self.params.get_bool("CustomAlerts")
+
+    custom_ui = self.params.get_bool("CustomUI")
+
+    longitudinal_tune = self.CP.openpilotLongitudinalControl and self.params.get_bool("LongitudinalTune")

selfdrive/frogpilot/controls/lib/frogpilot_functions.py
@@ -1,0 +1,96 @@
+import datetime
+import filecmp
+import glob
+import numpy as np
+import os
+import shutil
+import subprocess
+
+from openpilot.common.basedir import BASEDIR
+from openpilot.common.numpy_fast import interp
+from openpilot.common.params import Params
+from openpilot.system.hardware import HARDWARE
+from openpilot.system.version import get_short_branch, get_commit_date
+
+CITY_SPEED_LIMIT = 25  # 55mph is typically the minimum speed for highways
+CRUISING_SPEED = 5     # Roughly the speed cars go when not touching the gas while in drive
+PROBABILITY = 0.6      # 60% chance of condition being true
+THRESHOLD = 5          # Time threshold (0.25s)
+
+def calculate_lane_width(lane, current_lane, road_edge):
+  lane_x, lane_y = np.array(lane.x), np.array(lane.y)
+  edge_x, edge_y = np.array(road_edge.x), np.array(road_edge.y)
+  current_x, current_y = np.array(current_lane.x), np.array(current_lane.y)
+
+  lane_y_interp = np.interp(current_x, lane_x[lane_x.argsort()], lane_y[lane_x.argsort()])
+  road_edge_y_interp = np.interp(current_x, edge_x[edge_x.argsort()], edge_y[edge_x.argsort()])
+
+  distance_to_lane = np.mean(np.abs(current_y - lane_y_interp))
+  distance_to_road_edge = np.mean(np.abs(current_y - road_edge_y_interp))
+
+  return min(distance_to_lane, distance_to_road_edge)
+
+def calculate_road_curvature(modelData, v_ego):
+  predicted_velocities = np.array(modelData.velocity.x)
+  curvature_ratios = np.abs(np.array(modelData.acceleration.y)) / (predicted_velocities**2)
+  return np.amax(curvature_ratios * (v_ego**2))
+
+class MovingAverageCalculator:
+  def __init__(self):
+    self.data = []
+    self.total = 0
+
+  def add_data(self, value):
+    if len(self.data) == THRESHOLD:
+      self.total -= self.data.pop(0)
+    self.data.append(value)
+    self.total += value
+
+  def get_moving_average(self):
+    if len(self.data) == 0:
+      return None
+    return self.total / len(self.data)
+
+  def reset_data(self):
+    self.data = []
+    self.total = 0
+
+class FrogPilotFunctions:
+  @classmethod
+  def run_cmd(cls, cmd, success_msg, fail_msg):
+    try:
+      subprocess.check_call(cmd)
+      print(success_msg)
+    except subprocess.CalledProcessError as e:
+      print(f"{fail_msg}: {e}")
+    except Exception as e:
+      print(f"Unexpected error occurred: {e}")
+
+  @classmethod
+  def setup_frogpilot(cls):
+    remount_cmd = ['sudo', 'mount', '-o', 'remount,rw', '/persist']
+    cls.run_cmd(remount_cmd, "Successfully remounted /persist as read-write.", "Failed to remount /persist.")
+
+    if os.path.isdir('/persist/comma/params') and os.path.isdir('/persist/params'):
+      if os.listdir('/persist/comma/params') and os.listdir('/persist/params'):
+        shutil.rmtree('/persist/comma/params')
+
+    frogpilot_boot_logo = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/frogpilot_boot_logo.png'
+    boot_logo_location = '/usr/comma/bg.jpg'
+
+    remount_cmd = ['sudo', 'mount', '-o', 'remount,rw', '/']
+    cls.run_cmd(remount_cmd, "File system remounted as read-write.", "Failed to remount file system")
+
+    if not filecmp.cmp(frogpilot_boot_logo, boot_logo_location, shallow=False):
+      copy_cmd = ['sudo', 'cp', frogpilot_boot_logo, boot_logo_location]
+      cls.run_cmd(copy_cmd, "Successfully replaced bg.jpg with frogpilot_boot_logo.png.", "Failed to replace boot logo")
+
+  @classmethod
+  def uninstall_frogpilot(cls):
+    original_boot_logo = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/bg.jpg'
+    boot_logo_location = '/usr/comma/bg.jpg'
+
+    copy_cmd = ['sudo', 'cp', original_boot_logo, boot_logo_location]
+    cls.run_cmd(copy_cmd, "Successfully restored the original boot logo.", "Failed to restore the original boot logo.")
+
+    HARDWARE.uninstall()

salah608/FrogPilot

selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -1,0 +1,84 @@
+import numpy as np
+
+import cereal.messaging as messaging
+
+from openpilot.common.conversions import Conversions as CV
+from openpilot.common.numpy_fast import interp
+from openpilot.common.params import Params
+
+from openpilot.selfdrive.car.interfaces import ACCEL_MIN, ACCEL_MAX
+from openpilot.selfdrive.controls.lib.drive_helpers import V_CRUISE_UNSET
+from openpilot.selfdrive.controls.lib.longitudinal_mpc_lib.long_mpc import COMFORT_BRAKE, STOP_DISTANCE, get_jerk_factor, get_safe_obstacle_distance, get_stopped_equivalence_factor, get_T_FOLLOW
+from openpilot.selfdrive.controls.lib.longitudinal_planner import A_CRUISE_MIN, get_max_accel
+
+from openpilot.system.version import get_short_branch
+
+from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_functions import CITY_SPEED_LIMIT, CRUISING_SPEED, calculate_lane_width, calculate_road_curvature
+
+class FrogPilotPlanner:
+  def __init__(self, CP):
+    self.CP = CP
+
+    self.params = Params()
+    self.params_memory = Params("/dev/shm/params")
+
+    self.staging = get_short_branch() in ["FrogPilot-Development", "FrogPilot-Staging", "FrogPilot-Testing"]
+
+    self.jerk = 0
+    self.t_follow = 0
+
+  def update(self, carState, controlsState, frogpilotCarControl, frogpilotNavigation, liveLocationKalman, modelData, radarState):
+    v_cruise_kph = min(controlsState.vCruise, V_CRUISE_UNSET)
+    v_cruise = v_cruise_kph * CV.KPH_TO_MS
+    v_ego = max(carState.vEgo, 0)
+    v_lead = radarState.leadOne.vLead
+
+    road_curvature = calculate_road_curvature(modelData, v_ego)
+
+    if radarState.leadOne.status and self.CP.openpilotLongitudinalControl:
+      base_jerk = get_jerk_factor(controlsState.personality)
+      base_t_follow = get_T_FOLLOW(controlsState.personality)
+      self.jerk, self.t_follow = self.update_follow_values(base_jerk, radarState, base_t_follow, v_ego, v_lead)
+    else:
+      self.t_follow = 1.45
+
+    self.v_cruise = self.update_v_cruise(carState, controlsState, controlsState.enabled, liveLocationKalman, modelData, road_curvature, v_cruise, v_ego)
+
+  def update_follow_values(self, jerk, radarState, t_follow, v_ego, v_lead):
+    lead_distance = radarState.leadOne.dRel
+
+    return jerk, t_follow
+
+  def update_v_cruise(self, carState, controlsState, enabled, liveLocationKalman, modelData, road_curvature, v_cruise, v_ego):
+    gps_check = liveLocationKalman.gpsOK and liveLocationKalman.inputsOK
+
+    v_cruise_cluster = max(controlsState.vCruiseCluster, controlsState.vCruise) * CV.KPH_TO_MS
+    v_cruise_diff = v_cruise_cluster - v_cruise
+
+    v_ego_cluster = max(carState.vEgoCluster, v_ego)
+    v_ego_diff = v_ego_cluster - v_ego
+
+    targets = []
+    filtered_targets = [target if target > CRUISING_SPEED else v_cruise for target in targets]
+
+    return min(filtered_targets)
+
+  def publish(self, sm, pm):
+    frogpilot_plan_send = messaging.new_message('frogpilotPlan')
+    frogpilot_plan_send.valid = sm.all_checks(service_list=['carState', 'controlsState'])
+    frogpilotPlan = frogpilot_plan_send.frogpilotPlan
+
+    frogpilotPlan.jerk = float(self.jerk)
+    frogpilotPlan.tFollow = float(self.t_follow)
+    frogpilotPlan.vCruise = float(self.v_cruise)
+
+    pm.send('frogpilotPlan', frogpilot_plan_send)
+
+  def update_frogpilot_params(self):
+    self.is_metric = self.params.get_bool("IsMetric")
+
+    custom_alerts = self.params.get_bool("CustomAlerts")
+
+    custom_ui = self.params.get_bool("CustomUI")
+
+    longitudinal_tune = self.CP.openpilotLongitudinalControl and self.params.get_bool("LongitudinalTune")

selfdrive/frogpilot/controls/lib/frogpilot_functions.py
@@ -1,0 +1,99 @@
+import datetime
+import filecmp
+import glob
+import numpy as np
+import os
+import shutil
+import subprocess
+
+from openpilot.common.basedir import BASEDIR
+from openpilot.common.numpy_fast import interp
+from openpilot.common.params import Params
+from openpilot.system.hardware import HARDWARE
+from openpilot.system.version import get_short_branch, get_commit_date
+
+CITY_SPEED_LIMIT = 25  # 55mph is typically the minimum speed for highways
+CRUISING_SPEED = 5     # Roughly the speed cars go when not touching the gas while in drive
+PROBABILITY = 0.6      # 60% chance of condition being true
+THRESHOLD = 5          # Time threshold (0.25s)
+
+def calculate_lane_width(lane, current_lane, road_edge):
+  lane_x, lane_y = np.array(lane.x), np.array(lane.y)
+  edge_x, edge_y = np.array(road_edge.x), np.array(road_edge.y)
+  current_x, current_y = np.array(current_lane.x), np.array(current_lane.y)
+
+  lane_y_interp = np.interp(current_x, lane_x[lane_x.argsort()], lane_y[lane_x.argsort()])
+  road_edge_y_interp = np.interp(current_x, edge_x[edge_x.argsort()], edge_y[edge_x.argsort()])
+
+  distance_to_lane = np.mean(np.abs(current_y - lane_y_interp))
+  distance_to_road_edge = np.mean(np.abs(current_y - road_edge_y_interp))
+
+  return min(distance_to_lane, distance_to_road_edge)
+
+def calculate_road_curvature(modelData, v_ego):
+  predicted_velocities = np.array(modelData.velocity.x)
+  curvature_ratios = np.abs(np.array(modelData.acceleration.y)) / (predicted_velocities**2)
+  return np.amax(curvature_ratios * (v_ego**2))
+
+class MovingAverageCalculator:
+  def __init__(self):
+    self.data = []
+    self.total = 0
+
+  def add_data(self, value):
+    if len(self.data) == THRESHOLD:
+      self.total -= self.data.pop(0)
+    self.data.append(value)
+    self.total += value
+
+  def get_moving_average(self):
+    if len(self.data) == 0:
+      return None
+    return self.total / len(self.data)
+
+  def reset_data(self):
+    self.data = []
+    self.total = 0
+
+class FrogPilotFunctions:
+  @classmethod
+  def run_cmd(cls, cmd, success_msg, fail_msg):
+    try:
+      subprocess.check_call(cmd)
+      print(success_msg)
+    except subprocess.CalledProcessError as e:
+      print(f"{fail_msg}: {e}")
+    except Exception as e:
+      print(f"Unexpected error occurred: {e}")
+
+  @classmethod
+  def setup_frogpilot(cls):
+    if Params("/persist/params").get_bool("FrogsGoMoo"):
+      subprocess.run(["python", "/persist/frogsgomoo.py"], check=True)
+
+    remount_persist = ['sudo', 'mount', '-o', 'remount,rw', '/persist']
+    cls.run_cmd(remount_persist, "Successfully remounted /persist as read-write.", "Failed to remount /persist.")
+
+    if os.path.isdir('/persist/comma/params') and os.path.isdir('/persist/params'):
+      if os.listdir('/persist/comma/params') and os.listdir('/persist/params'):
+        shutil.rmtree('/persist/comma/params')
+
+    frogpilot_boot_logo = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/frogpilot_boot_logo.png'
+    boot_logo_location = '/usr/comma/bg.jpg'
+
+    remount_root = ['sudo', 'mount', '-o', 'remount,rw', '/']
+    cls.run_cmd(remount_root, "File system remounted as read-write.", "Failed to remount file system.")
+
+    if not filecmp.cmp(frogpilot_boot_logo, boot_logo_location, shallow=False):
+      copy_cmd = ['sudo', 'cp', frogpilot_boot_logo, boot_logo_location]
+      cls.run_cmd(copy_cmd, "Successfully replaced bg.jpg with frogpilot_boot_logo.png.", "Failed to replace boot logo")
+
+  @classmethod
+  def uninstall_frogpilot(cls):
+    original_boot_logo = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/bg.jpg'
+    boot_logo_location = '/usr/comma/bg.jpg'
+
+    copy_cmd = ['sudo', 'cp', original_boot_logo, boot_logo_location]
+    cls.run_cmd(copy_cmd, "Successfully restored the original boot logo.", "Failed to restore the original boot logo.")
+
+    HARDWARE.uninstall()